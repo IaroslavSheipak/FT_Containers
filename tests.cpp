802b4e038a13f03eef2db9e8c46acb5332d59e18
Strong Uzumaki <yara215@yandex.ru>
2021-10-13 23:43:48 +0300

    test of insert started

--- tests.cpp
+++ tests.cpp
@@ -1,7 +1,7 @@
-#include "vector/Vector.hpp"
+#include "../ft_containers/vector/Vector.hpp"
+#include <vector>
 #include <iostream>
 #include <algorithm>
-#include "vector/Vector.hpp"
 
 
 struct NoDefaultConstructor{
@@ -18,39 +18,57 @@ void output_vector(const ft::vector<T, Allocator> &v) {
 
 void	test_reserve(){
 	ft::vector<NoDefaultConstructor> vec;
-
 	try{
-		vec.reserve(-1);
+		vec.reserve(1);
+		return;
 	} catch (std::exception &a){
 		std::cout << a.what() << std::endl;
 	}
 }
 
-void test_erase() {
-	ft::vector<int> v(10);
-	for (size_t i = 0; i < v.size(); i++)
-		v[i] = i;
-	try {
-		std::cout << "vector before erasing:\n";
-		output_vector(v);
-		v.erase(v.begin() - 100);
-		std::cout << "vector after erasing:\n";
-		output_vector(v);
-	} catch (std::exception &e) {
-		std::cout << "some error happened: " << e.what() << std::endl;
-	}
+void test_insert(){
+
+	ft::vector<int> vec;
+
+	vec.insert(vec.begin(), 1);
+	vec.insert(vec.begin() + 1, 2);
+	std::cout << vec[0] << std::endl;
+	std::cout << vec[1] << std::endl;
+
 }
+
+void test_iterators(){
+
+	std::vector<int>			vec;
+	std::vector<int>::iterator	iter;
+
+	iter = vec.begin();
+
+
+
+}
+
+//void	test_erase() {
+//	ft::vector<int> v(10);
+//	for (size_t i = 0; i < v.size(); i++)
+//		v[i] = i;
+//	try {
+//		std::cout << "vector before erasing:\n";
+//		output_vector(v);
+//		v.erase(v.begin() - 100);
+//		std::cout << "vector after erasing:\n";
+//		output_vector(v);
+//	} catch (std::exception &e) {
+//		std::cout << "some error happened: " << e.what() << std::endl;
+//	}
+//}
+
 
 int main()
 {
-	ft::vector<int> a(5);
-	//a[0] = 42;
 	
 
 	test_reserve();
-	//a.push_back(2);
-	//a.push_back(3);
-	//std::sort(++a.begin(), a.end());
-	std::cout << a[0] << std::endl;
+	test_insert();
 	return (0);
 }
