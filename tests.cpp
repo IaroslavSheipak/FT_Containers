0c7060abd624bbc5e3cc818d2a2570c3df89371b
bentnn <kirill.ignatevv@gmail.com>
2021-10-13 20:52:33 +0300

    add some test erase

--- tests.cpp
+++ tests.cpp
@@ -9,6 +9,12 @@ struct NoDefaultConstructor{
 		~NoDefaultConstructor(){};
 };
 
+template<class T, class Allocator = std::allocator<T> >
+void output_vector(const ft::vector<T, Allocator> &v) {
+	for (size_t i = 0; i < v.size(); i++)
+		std::cout << v[i] << " ";
+	std::cout << std::endl;
+}
 
 void	test_reserve(){
 	ft::vector<NoDefaultConstructor> vec;
@@ -18,7 +24,21 @@ void	test_reserve(){
 	} catch (std::exception &a){
 		std::cout << a.what() << std::endl;
 	}
+}
 
+void test_erase() {
+	ft::vector<int> v(10);
+	for (size_t i = 0; i < v.size(); i++)
+		v[i] = i;
+	try {
+		std::cout << "vector before erasing:\n";
+		output_vector(v);
+		v.erase(v.begin() - 100);
+		std::cout << "vector after erasing:\n";
+		output_vector(v);
+	} catch (std::exception &e) {
+		std::cout << "some error happened: " << e.what() << std::endl;
+	}
 }
 
 int main()
