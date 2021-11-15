92129162632dd3fef82283e2e88d14080b436f2b
Strong Uzumaki <yara215@yandex.ru>
2021-11-15 23:04:21 +0300

    fuck

--- map/RBTree.hpp
+++ map/RBTree.hpp
@@ -1,22 +1,29 @@
+#pragma once
+#include <memory>
+
 template<class Value>
 struct Node{
-	typedef Value				value_type;
-	typedef value_type*			pointer;
-	typedef const value_type*	const_pointer;
-	typedef value_type&			reference;
-	typedef const_value_type&	const_reference;
+	typedef Node*			pointer;
 
-	pointer	data;
+	explicit Node(Value *srcval = 0) :	value(srcval),
+										parent(0),
+										left(0),
+										right(0),
+										is_black(false),
+										is_nil(0){}
+	
+	Value	*value;
 	Node*	parent;
 	Node*	left;
 	Node*	right;	
-	bool	red;
+	bool	is_black;
+	bool	is_nil;
 	
 	Node(pointer value, Node* node = NULL, Node* parent = NULL) :
-	   value(value), left(node), right(node), parent(parent), color(true) {}	
+	   value(value), left(node), right(node), parent(parent), is_black(false) {}	
 };
 
-template<class Value, class Compare = std::less<value>, class Alloc = std::allocator<Value> >
+template<class Value, class Compare = std::less<Value>, class Alloc = std::allocator<Value> >
 class RBTree{
 	public:
 		typedef Value value_type;
@@ -30,8 +37,8 @@ class RBTree{
 		typedef typename allocator_type::const_reference const_reference;
 		typedef typename allocator_type::pointer pointer;
 		typedef typename allocator_type::const_pointer const_pointer;
-		typedef ptrdiff_t	difference_type;
-		typedef std:size_t	size_type;
+		typedef std::ptrdiff_t	difference_type;
+		typedef std::size_t	size_type;
 		//typedef iterator;
 		//typedef const_iterator;
 		//typedef	reverse_iterator;
@@ -47,51 +54,51 @@ class RBTree{
 		size_type					_size;
 		
 		//HELPER FUNCTIONS
-		void 	_rotate_right(link	node){
+		void 	_rotate_right(node_pointer	node){
 			node_pointer y;
 
 			y = left(node);
 			if (right(y) != _nil)
 				parent(y->right) = node;
 			if (node == _root)
-				_root = tmp;
+				_root = y;
 			else if (node == right(node->parent))
-				right(node->parent) = tmp;
+				right(node->parent) = y;
 			else
-				left(node->parent) = tmp;
+				left(node->parent) = y;
 		}
-		void 	_rotate_left(link	node){
+		void 	_rotate_left(node_pointer	node){
 			node_pointer y;
 
 			y = right(node);
 			if (left(y) != _nil)
 				parent(y->left) = node;
 			if (node == _root)
-				_root = tmp;
+				_root = y;
 			else if (node == left(node->parent))
-				left(node->parent) = tmp;
+				left(node->parent) = y;
 			else
-				right(node->parent) = tmp;
+				right(node->parent) = y;
 		}
-		node_pointer	_insert(link	z){
+		node_pointer	_insert(node_pointer new_node){
 			if (!_root)
 				return(_root = new_node);
-			if (_compare(*new_node->content, *root->content)){
-				if (!is_nil(root->left))
-					return (_insert(root->left, new_node);
-				root->left = new_node;
+			if (_compare(*new_node->content, *_root->content)){
+				if (!is_nil(_root->left))
+					return (_insert(_root->left, new_node));
+				_root->left = new_node;
 			}
 			else{
-				if (!is_nil(root->right))
-					return (_insert(root->right, new_node);
-				root->right = new_node;
+				if (!is_nil(_root->right))
+					return (_insert(_root->right, new_node));
+				_root->right = new_node;
 			}
-			new_node->father = root;
+			new_node->father = _root;
 			return (new_node);
 		}
 
 		void _insert_fixup(node_pointer node){
-			if (node != _root && cur_node->father != _root){
+			if (node != _root && node->father != _root){
 				while (!node->father->is_black){
 					if (node->father == node->father->father->left){
 						node_pointer uncle = node->father->father->right;
@@ -115,37 +122,91 @@ class RBTree{
 			}
 			_root->is_black = true;
 		}
-		//PUBLIC FUNCTIONS
-	public:
-		node_pointer	search(const value_type &val) const
-		{
-			node_pointer x;
-			x = _root;
-			while (x != _nil){
-				if (!_compare(value(x), val) && !_compare(val, value(x)))
-					break;
-				else if (_compare(val, value(x))
-					x = left(x);
-				else
-					x = right(x);
-			}
-			return (x);
+
+		bool is_nil(node_pointer node) const {
+			return node == _nil || node == _header;
 		}
-		//CONSTRUCTORS
-		RBTree(const Compare& compare, const allocator_type& a = allocator_type()):
-			_root(0), _val_alloc(a), _node_alloc(node_allocator()), _compare(compare), _size(0){
+
+		void clear_node(node_pointer node){
+			if (node && !is_nil(node)) {
+				clear_node(node->right);
+				clear_node(node->left);
+			}
+				_val_alloc.destroy(node->value);
+				_val_alloc.deallocate(node->value, 1);
+				_node_alloc.deallocate(node, 1);
+		}
+	
+		void	init_nil_head(){
 			_nil = _node_alloc.allocate(1);
-			_node_alloc.construct(_nil, node<Content>());
+			_node_alloc.construct(_nil, Node<Value>());
 			_nil->is_black = true;
 			_nil->is_nil = true;
 			_header = _node_alloc.allocate(1);
-			_node_alloc.construct(_header, node<Value>());
-			_header->content = _con_alloc.allocate(1);
-			_val_alloc.construct(_header->content, Value());
-			}	
+			_node_alloc.construct(_header, Node<Value>());
+			_header->value = _val_alloc.allocate(1);
+			_val_alloc.construct(_header->value, Value());
+			_header->is_black = true;
+		}
 
 
+		//PUBLIC FUNCTIONS
+	public:
+		node_pointer	search(const value_type &value, node_pointer node) const
+		{
+			if(!node || is_nil(node))
+				return NULL;
+			if (_compare(value, *node->value))
+				return search(value, node->left);
+			if (_compare(*node->value, value))
+				return search(value, node->right);
+			return node;
+		}
+		
+		void /*pohui*/ insert(value_type *value){
+			node_pointer find_val = search(*value, _root);
+		} 
+		//CONSTRUCTORS
+		RBTree(const Compare &comp, const allocator_type& a = allocator_type()):
+				_root(0), _val_alloc(a), _node_alloc(node_allocator()), _compare(comp), _size(0){
+			init_nil_head();
+			_root = _header;
+		}
 
-}
+		RBTree() : _root(0), _val_alloc(allocator_type()), _node_alloc(node_allocator()), _compare(key_compare()), _size(0){
+			init_nil_head();
+			_root = _header;
+		}
+
+		RBTree(const RBTree& src){
+			*this = src;
+		}
+		
+		RBTree& operator=(const RBTree & src){
+			this->_node_alloc = src._node_alloc;
+			this->_val_alloc = src._val_alloc;
+			this->_cmp = src._cmp;
+			clear_node(_root);
+			if (this->_nil == NULL)
+			init_nil_head();
+			if (src._size == 0)
+			this->_root = this->_header;
+			else {
+				this->_root = copy_node(src._root);
+				copy_child(this->_root, src._root);
+			}
+			this->_size = src._size;
+			return *this;
+		}
+
+		~RBTree(){
+			clear_node(_root);
+			_val_alloc.destroy(_header->value);
+			_val_alloc.deallocate(_header->value, 1);
+			_node_alloc.deallocate(_nil, 1);
+			_node_alloc.deallocate(_header, 1);
+		}
+
+};
 
 
